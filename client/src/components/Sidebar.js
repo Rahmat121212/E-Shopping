45dc37e0f7850f1b6473b2700cac51a63ea79843
Rahmat ullah <baheer224@gmail.com>
2023-07-29 19:15:30 +0500

    add

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -3,10 +3,11 @@ import { CgDisplayFlex } from 'react-icons/cg';
 import { TbBrandAirbnb } from 'react-icons/tb';
 import { VscFeedback } from 'react-icons/vsc';
 import { RiAdminLine } from 'react-icons/ri';
+import { AiOutlineDashboard } from 'react-icons/ai';
 const Sidebar = ({ side, closeSidebar }) => {
   return (
     <div
-      className={`fixed top-0 ${side} sm:left-0 w-64 h-screen bg-gray-800 z-10 transition-all`}
+      className={`fixed top-0 ${side} sm:left-0 w-64 h-screen bg-gray-800 z-10 transition-all `}
     >
       <i
         className="bi bi-x-lg absolute top-4 right-4 sm:hidden text-white block cursor-pointer text-lg"
@@ -15,7 +16,13 @@ const Sidebar = ({ side, closeSidebar }) => {
      
         <img src="/logo.jpg" alt="logo" className="bg-white w-[50%] h-[20%] bg mx-14 object-cover my-7 felx justify-center items-center rounded-[90%]" />
      
-      <ul className="mt-4">
+      <ul className="mt-4 ">
+        <li className="px-4 cursor-pointer transition-all py-3 text-white flex items-center hover:bg-gray-600">
+         <AiOutlineDashboard className="mr-2 inline-block text-lg  "  />
+          <Link to="/dashboard/Dashboard" className="text-base capitalize">
+            Dashboard
+          </Link>
+        </li>
         <li className="px-4 cursor-pointer transition-all py-3 text-white flex items-center hover:bg-gray-600">
          <RiAdminLine className="mr-2 inline-block text-lg  "  />
           <Link to="/dashboard/admin" className="text-base capitalize">
